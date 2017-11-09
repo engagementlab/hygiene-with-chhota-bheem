b78e1cb2c9a4a21188f9726ff6bc5960c2918324
Johnny Richardson <johnny.c.rich@gmail.com>
2017-11-09 17:13:31 -0500

    Began work on scenelimiter editor utility for more precise object cleanup.

---
+++ Assets/Scripts/SceneLimiter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLimiter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		Debug.Log(other.name);
+		
+		var archetypeMove = other.GetComponent<ArchetypeMove>();
+		if(archetypeMove != null)
+			Destroy(other.gameObject);
+	}
+}
