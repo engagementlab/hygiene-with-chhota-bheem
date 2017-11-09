2856105f59d684b440ac2b84360341448bbf2ffa
esalling23 <esalling23@gmail.com>
2017-11-09 14:45:06 -0500

    integrate playerprefs to save sound/music/volume preferences for players across scenes/games

--- Assets/Scripts/MenuUI.cs
+++ Assets/Scripts/MenuUI.cs
@@ -7,7 +7,6 @@ using UnityEngine.UI;
 public class MenuUI : MonoBehaviour
 {
 
-	public Animator SettingsAnimator;
 	public AudioClip MenuMusic;
 	private AudioSource _audio;
 
@@ -16,6 +15,8 @@ public class MenuUI : MonoBehaviour
 
 	private Toggle SoundToggle;
 	private Toggle MusicToggle;
+
+	private Slider VolumeSlider;
 	
 	// Use this for initialization
 	void Start () {
@@ -24,13 +25,15 @@ public class MenuUI : MonoBehaviour
 		// Start menu music
 		_audio.PlayOneShot(MenuMusic);
 		
-		// Find Toggles
+		// Find toggles and sliders
 		SoundToggle = gameObject.transform.Find("Settings/Board/Sound/Toggle").GetComponent<Toggle>();
 		MusicToggle = gameObject.transform.Find("Settings/Board/Music/Toggle").GetComponent<Toggle>();
+		VolumeSlider = gameObject.transform.Find("Settings/Board/Volume/Slider").GetComponent<Slider>();
 
-		// Set Toggles to GameConfig settings
-		SoundToggle.isOn = GameConfig.SoundOn;
-		MusicToggle.isOn = GameConfig.MusicOn;
+		// Set toggles and sliders to player pref settings
+		SoundToggle.isOn = PlayerPrefs.GetInt("sound") == 1;
+		MusicToggle.isOn = PlayerPrefs.GetInt("music") == 1;
+		VolumeSlider.value = PlayerPrefs.GetFloat("volume");
 	}
 
 	public void Screen(int num)
@@ -43,16 +46,27 @@ public class MenuUI : MonoBehaviour
 		gameObject.GetComponent<Animator>().SetInteger("LevelSelected", num);
 	}
 
+	public void Volume(float volume)
+	{
+		if (volume > 1f)
+			volume = 1f;
+		
+		GameConfig.GlobalVolume = volume;
+		GameConfig.UpdatePrefs("volume", null, volume);
+		Debug.Log(GameConfig.GlobalVolume);
+	}
+
 	public void Sound()
 	{
 		GameConfig.SoundOn = !GameConfig.SoundOn;
+		GameConfig.UpdatePrefs("sound", GameConfig.SoundOn == true ? 1 : 0);
 		Debug.Log(GameConfig.SoundOn);
 	}
 
 	public void Music()
 	{
 		GameConfig.MusicOn = !GameConfig.MusicOn;
-
+		GameConfig.UpdatePrefs("music", GameConfig.MusicOn == true ? 1 : 0);
 		Debug.Log(GameConfig.MusicOn);
 	}
 }
