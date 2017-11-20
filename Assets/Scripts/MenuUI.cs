fa759975478f2423c3f4912f67c4febd6304de7a
Johnny Richardson <johnny.c.rich@gmail.com>
2017-11-20 16:07:45 -0500

    Fix for bubble interval doubling on level restart.

--- Assets/Scripts/MenuUI.cs
+++ Assets/Scripts/MenuUI.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.VR.WSA.WebCam;
 
 public class MenuUI : MonoBehaviour
 {
