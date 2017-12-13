5ae79028ab72e50eba2c7de25f148d5f85c9f7a7
Erica Salling <esalling23@gmail.com>
2017-12-13 15:01:27 -0500

    Villagers use final sprite frame when “cleaned” and change frames based on hit points and player strength

--- Assets/Scripts/VillagerObject.cs
+++ Assets/Scripts/VillagerObject.cs
@@ -16,6 +16,9 @@ public class VillagerObject : ArchetypeMove
 	
 	// Rate for reducing particle cloud on hit
 	private float _rate;
+	
+	// Rate for sprite sheet frame swapping
+	private int _frame;
 
 	private Particles _particles;
 	private ParticleSystem _particleSystem;
@@ -80,7 +83,7 @@ public class VillagerObject : ArchetypeMove
 		
 		if(_particleReady)
 			_particles = GetComponent<Particles>();
-
+		
 	}
 
 	private void Start()
@@ -91,7 +94,8 @@ public class VillagerObject : ArchetypeMove
 		// Pick random villager spritesheet and set to first frame
 		_spriteFrames = Resources.LoadAll<Sprite>("Villagers/"+UnityEngine.Random.Range(1, 4));
 		_villagerRenderer.sprite = _spriteFrames[0];
-		
+		_frame = _spriteFrames.Length / HitPoints;
+
 		if (_particleSystem != null && _particleReady)
 			_particleSystem.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
 		
@@ -138,14 +142,16 @@ public class VillagerObject : ArchetypeMove
 			if (_particleReady)
 				_particles.ParticleReduce(_rate);
 
-			if(_bubblesHit < _spriteFrames.Length)
+			if(_bubblesHit < HitPoints)
 			{
 				bool hiSound = Random.value > .5f;
 				Events.instance.Raise(SoundEvent.WithClip(_playerScript.BubbleSounds[hiSound?0:1]));
-				_villagerRenderer.sprite = _spriteFrames[_bubblesHit];
+				_villagerRenderer.sprite = _spriteFrames[_frame * _bubblesHit];
 			}
 			return;
 		}
+		
+		_villagerRenderer.sprite = _spriteFrames[_spriteFrames.Length - 1];
 		
 		Events.instance.Raise (new ScoreEvent(pointsWorth));
 
