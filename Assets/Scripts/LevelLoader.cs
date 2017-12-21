0b6177b244469203f9c1017e4f333f62871585ed
Johnny Richardson <johnny.c.rich@gmail.com>
2017-12-21 17:18:06 -0500

    Preloader cleanup.

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -5,16 +5,14 @@ using UnityEngine.UI;
 
 public class LevelLoader : MonoBehaviour {
 
-	public Slider slider;
+	public Slider Slider;
 	public RectTransform Logo;
-	AsyncOperation sceneAO;
+	public Image Text;
+	private AsyncOperation _sceneAo;
 	
 	// Use this for initialization
-	void Start ()
+	private void Start ()
 	{
-
-//		slider = transform.Find("Slider").GetComponent<Slider>();
-		
 		Events.instance.AddListener<LoadLevelEvent> (LoadLevel);
 		
 	}
@@ -24,7 +22,6 @@ public class LevelLoader : MonoBehaviour {
 		Events.instance.RemoveListener<LoadLevelEvent> (LoadLevel);
 
 	}
-
 	
 	void LoadLevel(LoadLevelEvent e)
 	{
@@ -56,31 +53,35 @@ public class LevelLoader : MonoBehaviour {
 			GameConfig.DictLostCount = new Dictionary<string, int>{{baseName, 0}};
 		
 		StartCoroutine(LoadingSceneRealProgress(baseName));
+		
 	}
 	
 	IEnumerator LoadingSceneRealProgress(string sceneName)
 	{
+		
 		GetComponent<Image>().enabled = true;
-		slider.gameObject.SetActive(true);
+		Slider.gameObject.SetActive(true);
 		Logo.gameObject.SetActive(true);
+		Text.gameObject.SetActive(true);
 
-		iTween.PunchRotation(Logo.gameObject, iTween.Hash("amount", new Vector3(0, 0, 7), "time", 1, "easetype", iTween.EaseType.easeInOutBounce));
+		iTween.ScaleFrom(Logo.gameObject, iTween.Hash("scale", Vector3.zero, "time", 1, "easetype", iTween.EaseType.easeOutBack));
 		
 		yield return new WaitForSeconds(1);
 		
-		sceneAO = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName);
+		_sceneAo = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName);
 
 		// disable scene activation while loading to prevent auto load
-		sceneAO.allowSceneActivation = false;
+		_sceneAo.allowSceneActivation = false;
 
-		while (!sceneAO.isDone) {
-			slider.value = sceneAO.progress;
+		while (!_sceneAo.isDone) {
+			Slider.value = _sceneAo.progress;
 
-			if (sceneAO.progress >= .9f) {
-				slider.value = 1f;
-				sceneAO.allowSceneActivation = true;
+			if (_sceneAo.progress >= .9f) {
+				Slider.value = 1f;
+				_sceneAo.allowSceneActivation = true;
 			}
 			yield return null;
 		}
+		
 	}
 }
