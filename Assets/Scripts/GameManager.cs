6a985c3fa6785ff98292c1799bb3825ccd39d414
Johnny Richardson <johnny.c.rich@gmail.com>
2018-01-17 18:29:02 -0500

    Menu fix, publishing assets.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -107,29 +107,29 @@ public class GameManager : MonoBehaviour
 		if(!Debug.isDebugBuild) return;
 		#endif
 		
-		int w = Screen.width, h = Screen.height;
- 
-		GUIStyle style = new GUIStyle();
- 
-		Rect rect = new Rect(0, 0, w, h * 2 / 100);
-		style.alignment = TextAnchor.UpperLeft;
-		style.fontSize = h * 2 / 100;
-		style.normal.textColor = Color.white;
-		float msec = _deltaTime * 1000.0f;
-		float fps = 1.0f / _deltaTime;
-		string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
-		
-		GUI.Label(rect, text, style);
-		if(GUI.Button(new Rect(0, 40, 100, 50), "Stress Test"))
-		{
-			for(var i = 0; i < 45; i++)
-			{
-				Instantiate(VillagerPrefab, new Vector3(Random.Range(-2, 2), Random.Range(0, 20), 0), Quaternion.identity);
-			}
-		}
-		
-		// God mode toggle
-		GameConfig.GodMode = GUI.Toggle(new Rect(0, 100, 100, 50), GameConfig.GodMode, "God Mode");
+//		int w = Screen.width, h = Screen.height;
+// 
+//		GUIStyle style = new GUIStyle();
+// 
+//		Rect rect = new Rect(0, 0, w, h * 2 / 100);
+//		style.alignment = TextAnchor.UpperLeft;
+//		style.fontSize = h * 2 / 100;
+//		style.normal.textColor = Color.white;
+//		float msec = _deltaTime * 1000.0f;
+//		float fps = 1.0f / _deltaTime;
+//		string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+//		
+//		GUI.Label(rect, text, style);
+//		if(GUI.Button(new Rect(0, 40, 100, 50), "Stress Test"))
+//		{
+//			for(var i = 0; i < 45; i++)
+//			{
+//				Instantiate(VillagerPrefab, new Vector3(Random.Range(-2, 2), Random.Range(0, 20), 0), Quaternion.identity);
+//			}
+//		}
+//		
+//		// God mode toggle
+//		GameConfig.GodMode = GUI.Toggle(new Rect(0, 100, 100, 50), GameConfig.GodMode, "God Mode");
 
 	}
 
