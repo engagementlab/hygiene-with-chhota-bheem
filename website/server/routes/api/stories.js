49de5231f4f862e539cc2e280bd5e0b67d0cf303
Johnny Richardson <johnny.c.rich@gmail.com>
2019-05-15 13:47:56 -0400

    Added all Hindi fields for CMS, updated api endpoints to support hindi, stories layout edits.

--- website/server/routes/api/stories.js
+++ website/server/routes/api/stories.js
@@ -8,30 +8,47 @@
  *
  * ==========
  */
-const keystone =  global.keystone,
+const keystone = global.keystone,
     mongoose = require('mongoose'),
     Bluebird = require('bluebird');
-    mongoose.Promise = require('bluebird');
-    
-    let list = keystone.list('Story').model;
+mongoose.Promise = require('bluebird');
+
+let list = keystone.list('Story').model;
 
 
 var getAdjacent = (results, res, lang) => {
-    
+
     let fields = 'key photo.public_id ';
-    fields += (lang === 'en') ? 'name' : 'nameTm';
+
+    if (lang === 'en')
+        fields += 'name';
+    else if (lang === 'tm')
+        fields += 'nameTm';
+    else if (lang === 'hi')
+        fields += 'nameHi';
 
     // Get one next/prev person from selected person's sortorder
-    let nextPerson = list.findOne({sortOrder: {
-        $gt: results.jsonData.sortOrder
-    }}, fields).limit(1);
-    let prevPerson = list.findOne({sortOrder: {
-        $lt: results.jsonData.sortOrder
-    }}, fields).sort({sortOrder: -1}).limit(1);
-    
+    let nextPerson = list.findOne({
+        sortOrder: {
+            $gt: results.jsonData.sortOrder
+        }
+    }, fields).limit(1);
+    let prevPerson = list.findOne({
+        sortOrder: {
+            $lt: results.jsonData.sortOrder
+        }
+    }, fields).sort({
+        sortOrder: -1
+    }).limit(1);
+
     // Poplulate next/prev and output response
-    Bluebird.props({next: nextPerson, prev: prevPerson}).then(nextPrevResults => {
-        let output = Object.assign(nextPrevResults, {person: results.jsonData});
+    Bluebird.props({
+        next: nextPerson,
+        prev: prevPerson
+    }).then(nextPrevResults => {
+        let output = Object.assign(nextPrevResults, {
+            person: results.jsonData
+        });
         return res.status(200).json({
             status: 200,
             data: output
@@ -45,31 +62,39 @@ var getAdjacent = (results, res, lang) => {
 var buildData = (storyId, res, lang) => {
 
     let data = null;
-    let fields = 'key nameTm subtitleTm photo.public_id';
-    if(lang === 'en')
-        fields = 'key name subtitle photo.public_id';
-        
-        
-        if(storyId) {
-            let subFields = ' descriptionTm.html ';
-            if(lang === 'en')
-            subFields = ' description.html ';
-            
-            console.log(storyId)
-        data = list.findOne({ key: storyId }, fields + subFields + 'sortOrder -_id');
-    }
-    else 
-        data = list.find({}, fields + ' -_id').sort([['sortOrder', 'descending']]);
+    let fields = 'key photo.public_id ';
+
+    if (lang === 'en')
+        fields += 'name subtitle';
+    else if (lang === 'tm')
+        fields += 'nameTm subtitleTm';
+    else if (lang === 'hi')
+        fields += 'nameHi subtitleHi';
+
+    if (storyId) {
+        let subFields = ' description.html ';
+        if (lang === 'tm')
+            subFields = ' descriptionTm.html ';
+        else if (lang === 'hi')
+            subFields = ' descriptionHi.html ';
+
+
+        data = list.findOne({
+            key: storyId
+        }, fields + subFields + 'sortOrder -_id');
+    } else
+        data = list.find({}, fields + ' -_id').sort([
+            ['sortOrder', 'descending']
+        ]);
 
     Bluebird.props({
             jsonData: data
         })
         .then(results => {
             // When retrieving one story, also get next/prev ones
-            if(storyId)
+            if (storyId)
                 getAdjacent(results, res, lang);
-            else
-            {
+            else {
                 return res.status(200).json({
                     status: 200,
                     data: results.jsonData
