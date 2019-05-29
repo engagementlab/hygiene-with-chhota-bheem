353c0ef65336dddec91882df3b7039fcf16a8e05
Johnny Richardson <johnny.c.rich@gmail.com>
2019-05-29 17:34:40 -0400

    Fixes for most issues in #453.

--- website/client/src/app/app.component.ts
+++ website/client/src/app/app.component.ts
@@ -42,18 +42,14 @@ export class AppComponent implements OnInit, AfterViewInit {
     
     this._router.events.subscribe((evt) => {
 
-      if(evt instanceof NavigationEnd) {
-
-        // Close menu
-        document.getElementById('body').classList.remove('open');
-        document.getElementById('menu').classList.remove('open');
-        document.getElementById('menu-btn').classList.remove('open');
-        
-      }
-
       if (!(evt instanceof NavigationEnd))
         return;
     
+      // Close menu
+      document.getElementById('body').classList.remove('open');
+      document.getElementById('menu').classList.remove('open');
+      document.getElementById('menu-btn').classList.remove('open');
+        
       // Always go to top of page
       window.scrollTo(0, 0);
 
